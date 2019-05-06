b777aee9eca0d76b68003d8a82cb5f3330b1d4f4
KDavies2020 <kdavies311@gmail.com>
2019-05-06 15:53:39 -0700

    project completed. Included notes on breaking down counter and redux flow of data.

--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -6,18 +6,36 @@ function Counter(props) {
         <div>
             <h1>I am a counter!</h1>
             <p>Count: {props.count}</p>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button onClick={props.onIncrementClick}>Increment</button>
+      <button onClick={props.onDecrementClick}>Decrement</button>
         </div>
     )
 }
 //this function is going where we are assigning a value to state. 
+//these are actions that really belong in action file or folder. 
+// ok for small scale. Notice that action.type = is just used to name 
+//the action. nothing more. 
 function mapStateToProps (state) {
     console.log('mapStateToProps', state)
     return {
         count: state.count
     }
+};
+
+function mapDispatchToProps (dispatch) {
+    return {
+        onIncrementClick: () => {
+            const action = { type: "INCREMENT"};
+            dispatch(action)
+        },
+        onDecrementClick: () => {
+            const action = { type: "DECREMENT"};
+            dispatch(action)
+        }
+    }
 }
+
+    
 //connect is used below through react-redux. provides a redux wrapper
 //on our traditional react component. 
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- counter/src/store/index.js
+++ counter/src/store/index.js
@@ -5,8 +5,16 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-    console.log('reducer running', action)
-    return state;
+    console.log('reducer running', action);
+
+    switch (action.type) {
+        case "INCREMENT" :
+        return Object.assign({}, state, {count: state.count+1});
+        case "DECREMENT" :
+        return Object.assign({}, state, {count: state.count - 1});
+        default: return state
+    }
+    
 }
 
 
