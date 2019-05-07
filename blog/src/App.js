6c02a3359b65a8f407755279be55e75741ce6158
KDavies2020 <kdavies311@gmail.com>
2019-05-07 11:42:23 -0700

    Title and body inputs and text areas completed. Need to add onlick / onsubmit functionality.

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Post from './components/post';
+import Postform from './components/postform';
 
 function App() {
   return (
     <div className="App">
+    <Postform />
+    <hr />
       <Post />
     </div>
+    
+    
   );
 }
 
